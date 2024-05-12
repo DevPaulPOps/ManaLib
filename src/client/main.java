e6b89c894950af0703f5b5e9f2995eeebee9c947
paulpp78 <perigault.paul@gmail.com>
2024-05-12 14:33:23 +0200

    Ajout des classes non codé pour le client

---
+++ src/client/main.java
@@ -0,0 +1,6 @@
+package client;
+
+public class main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/client/manageClientServer.java
@@ -0,0 +1,5 @@
+package client;
+
+public class manageClientServer {
+    private clientServer clientServer;
+}
