3fbc12d15ff1fc6dab9963ee9d552c58233690fe
paulpp78 <paulperigault@wevii.net>
2024-05-22 12:11:43 +0200

    Ajout du client server

--- src/main/java/client/clientServer.java
+++ src/main/java/client/clientServer.java
@@ -1,6 +1,21 @@
 package client;
 //TODO implement this class
 
-public class clientServer {
+import server.serv.bttp.BttpProtocole;
 
+import java.io.IOException;
+import java.net.Socket;
+
+public class clientServer {
+    private final int port = 3000;
+    private final String host = "localhost";
+    private final BttpProtocole bttpProtocole;
+
+    public clientServer(String host, int port) throws IOException {
+        this.bttpProtocole = new BttpProtocole(new Socket(host, port));
+    }
+
+    public void close() {
+        bttpProtocole.close();
+    }
 }
